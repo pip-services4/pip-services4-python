447b777ab59aeb9d3dded456d559fe7278c2ad41
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-07-24 14:05:44 +0300

    added config

---
+++ pip-services4-config-python/example.py
@@ -0,0 +1,42 @@
+import threading
+import time
+import random
+
+
+class Counter(object):
+    def __init__(self, start=0):
+        self.lock = threading.Lock()
+        self.value = start
+
+    def increment(self):
+        print('Waiting for a lock')
+        self.lock.acquire()
+        try:
+            print('Acquired a lock')
+            self.value = self.value + 1
+        finally:
+            print('Released a lock')
+            self.lock.release()
+
+
+def worker(c):
+    for i in range(2):
+        r = random.random()
+        print(f'Sleeping {r}')
+        time.sleep(r)
+        c.increment()
+    print('Done')
+
+
+if __name__ == '__main__':
+    counter = Counter()
+    for i in range(2):
+        t = threading.Thread(target=worker, args=(counter,))
+        t.start()
+
+    print('Waiting for worker threads')
+    main_thread = threading.currentThread()
+    for t in threading.enumerate():
+        if t is not main_thread:
+            t.join()
+    print(f'Counter: {counter.value}')

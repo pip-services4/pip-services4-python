c7aa02ca930f1a8c316dc29c2222d2120d3ea4b7
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-07-28 21:02:41 +0300

    added rpc

---
+++ pip-services4-rpc-python/test/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    pip_services4_rpc.clients.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Clients module implementation
+
+    :copyright: Conceptual Vision Consulting LLC 2015-2016, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__all__ = ['Dummy', 'DummyCommandSet', 'DummyController', 'IDummyController']
+
+from test.sample.Dummy import Dummy

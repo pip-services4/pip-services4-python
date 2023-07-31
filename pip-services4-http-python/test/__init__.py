0876eff21d3cce4a7b110b962142908de3a9f53f
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-07-31 13:56:06 +0300

    added http

---
+++ pip-services4-http-python/test/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    pip_services4_http.clients.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Clients module implementation
+
+    :copyright: Conceptual Vision Consulting LLC 2015-2016, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__all__ = ['Dummy', 'DummyCommandSet', 'DummyService', 'IDummyService']
+
+from .Dummy import Dummy
+from .DummyCommandSet import DummyCommandSet
+from .DummyService import DummyService
+from .IDummyService import IDummyService
